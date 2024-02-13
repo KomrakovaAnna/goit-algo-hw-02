345fa8ac41c6e71c7c0a3e186bcc703b2618bb2b
KomrakovaAnna <komrakova.anna@gmail.com>
2024-02-13 14:19:05 +0000

    small improvement

--- task2_deque.py
+++ task2_deque.py
@@ -1,18 +1,21 @@
 from collections import deque
 
 def is_palindrome(string_to_test):
-    if not string_to_test:
-        return print("Рядок пустий")
         
     string_to_test = string_to_test.lower().replace(" ", "")
     queue = deque()
 
     for i in string_to_test:
         queue.append(i)
-
+        
+    if len(queue) < 1:
+        return False
+    
     while len(queue) > 1:
+        
         if queue.popleft() != queue.pop():
             return False
+        
     return True
 
 string_to_test = input("Введіть рядок >>> ")
