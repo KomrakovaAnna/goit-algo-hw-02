c4719389a5ea3afbb9e5001daaaadbb627d06f35
KomrakovaAnna <komrakova.anna@gmail.com>
2024-02-13 00:04:25 +0000

    task1

---
+++ task1_queue.py
@@ -0,0 +1,28 @@
+from queue import Queue
+import random
+
+queue = Queue()
+
+def generate_request():
+    new_request = random.randint(1, 5)
+    queue.put(new_request)
+    print(f"New request has been generated {new_request} added to the queue")
+
+def process_request():
+    if queue.empty():
+        print("Queue is empty")
+    else:
+        request = queue.get()
+        print(f"Processed: {request}")
+
+while True:
+    choice = input("Print generate to generate request, process to process requst, stop to quit >>> ").lower()
+    if choice == "generate":
+        generate_request()
+    elif choice == "process":
+        process_request()
+    elif choice == "stop":
+        print("Bye!")
+        break
+    else:
+        print("Incorrect input. Try again. Press 1 to generate request, 2 to process requst, Stop to quit")
